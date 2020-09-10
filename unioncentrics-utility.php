db4c357174a75f6ec54d7429bd28ad5533cc4675
trav <trvswgnr@gmail.com>
2020-09-09 23:32:12 -0400

    add author email

--- unioncentrics-utility.php
+++ unioncentrics-utility.php
@@ -8,7 +8,7 @@
  * Author URI: http://unioncentrics.com
  *
  * @package unioncentrics-utility
- * @author Travis Aaron Wagner
+ * @author Travis Aaron Wagner <travis@travisaw.com>
  *
  * @todo move functions to class.
  */
